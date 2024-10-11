22b89254deff89f1a846370a54d5dfebea3c2012
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-10 23:55:55 -0600

    JWT service global

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-
+import { LoginUserDto } from './dto/login-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -14,7 +13,7 @@ export class AuthController {
   }
 
   @Post("login")
-  login(@Body() createUserDto: CreateUserDto){
-    return  this.authService.loginUser(createUserDto)
+  login(@Body() loginUserDto: LoginUserDto){
+    return  this.authService.loginUser(loginUserDto)
   }
 }
