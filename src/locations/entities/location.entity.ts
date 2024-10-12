c3610270552e3cf0682680cdb5b356771fd781ae
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-12 02:06:08 -0600

    More implementation swagger

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -2,15 +2,28 @@ import { Employee } from "src/employees/entities/employee.entity";
 import { Manager } from "src/managers/entities/manager.entity";
 import { Region } from "src/regions/entities/region.entity";
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class Location {
   @PrimaryGeneratedColumn('increment')
   locationId: number;
+
+  @ApiProperty({
+    default: "OCSO Juriquilla"
+  })
   @Column('text')
   locationName: string;
+
+  @ApiProperty({
+    default: "Avenida Tal, S/N, 76220"
+  })
   @Column('text')
   locationAddress: string;
+
+  @ApiProperty({
+    default: [12, 12]
+  })
   @Column('simple-array')
   locationLatLng: number[];
 
