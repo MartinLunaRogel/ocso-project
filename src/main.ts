28c1d41a1e78b59ac212f5a2c62156e2f96bc088
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-19 01:15:39 -0600

    feat: Enable cors for front

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,24 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-     .setTitle('Ocso API')
-     .setDescription('Api for ocso management')
-     .setVersion('0.9')
-     .addBearerAuth()
-     .build();
-   const document = SwaggerModule.createDocument(app, config);
-   SwaggerModule.setup('api', app, document);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.allowedOrigin
+    }
+  });  const config = new DocumentBuilder()
+    .setTitle('Ocso API')
+    .setDescription('Api for ocso management')
+    .setVersion('0.9')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
   }))
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
