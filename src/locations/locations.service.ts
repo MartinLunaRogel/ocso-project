9f6fccc0c05576221bcba5d703dc7f721273b513
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-10-31 19:26:04 -0600

    Manager changes for location

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,42 +1,63 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CreateLocationDto } from './dto/create-location.dto';
-import { UpdateLocationDto } from './dto/update-location.dto';
-import { Location } from './entities/location.entity';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateLocationDto } from "./dto/create-location.dto";
+import { UpdateLocationDto } from "./dto/update-location.dto";
+import { Location } from "./entities/location.entity";
+import { Manager } from "src/managers/entities/manager.entity";
 
 @Injectable()
 export class LocationsService {
   constructor(
     @InjectRepository(Location)
-    private locationRepository: Repository<Location>
-  ){}
+    private locationRepository: Repository<Location>,
+    @InjectRepository(Manager)
+    private managerRepository: Repository<Manager>,
+  ) {}
   create(createLocationDto: CreateLocationDto) {
     return this.locationRepository.save(createLocationDto);
   }
 
   findAll() {
-    return this.locationRepository.find()
+    return this.locationRepository.find();
   }
 
-  findOne(id: number) {
-    const location = this.locationRepository.findOneBy({
+  async findOne(id: number) {
+    const location = await this.locationRepository.findOneBy({
       locationId: id,
-    })
-    if (!location) throw new NotFoundException("Location not found")
+    });
+    if (!location) throw new NotFoundException("Location not found");
+    return location;
   }
 
   async update(id: number, updateLocationDto: UpdateLocationDto) {
+    // Set manager to null
+    this.managerRepository
+      .createQueryBuilder()
+      .update()
+      .set({ location: null })
+      .where("locationId = :id", {
+        id,
+      }).execute();
+
     const location = await this.locationRepository.preload({
       locationId: id,
       ...updateLocationDto,
+    });
+    const savedLocation = await this.locationRepository.save(location);
+
+    const updatedManager = await this.managerRepository.preload({
+      managerId: updateLocationDto.manager,
+      location: location,
     })
-    return this.locationRepository.save(location)
+    this.managerRepository.save(updatedManager);
+
+    return savedLocation;
   }
 
   remove(id: number) {
     return this.locationRepository.delete({
-      locationId: id
-    })
+      locationId: id,
+    });
   }
 }
--- src/regions/regions.service.ts
+++ src/regions/regions.service.ts
@@ -28,6 +28,7 @@ export class RegionsService {
       regionId: id,
     });
     if (!region) throw new NotFoundException("Region not found");
+    return region;
   }
 
   async update(id: number, updateRegionDto: UpdateRegionDto) {
